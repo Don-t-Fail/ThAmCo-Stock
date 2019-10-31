044a1a28ba1f5902f93e46ea73c26825e7ff6562
Stedoss <29103029+Stedoss@users.noreply.github.com>
2019-10-31 16:32:18 +0000

    Initial commit for base MVC project.

---
+++ ThAmCo.Stock/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThAmCo.Stock.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
