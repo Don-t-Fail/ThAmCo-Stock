ba47093d51e7bf6299b91e24de5b3700167432a5
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-12-20 02:35:41 +0000

    Modify StockController to use a Dependency Injected context.

--- ThAmCo.Stock/Controllers/StockController.cs
+++ ThAmCo.Stock/Controllers/StockController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using ThAmCo.Stock.Data;
+using ThAmCo.Stock.Data.StockContext;
 using ThAmCo.Stock.Models.Dto;
 
 namespace ThAmCo.Stock.Controllers
@@ -15,10 +16,10 @@ namespace ThAmCo.Stock.Controllers
     [ApiController]
     public class StockController : Controller
     {
-        private readonly StockDbContext _context;
+        private readonly IStockContext _context;
         private readonly IHttpClientFactory _clientFactory;
 
-        public StockController(StockDbContext context, IHttpClientFactory httpClientFactory)
+        public StockController(IStockContext context, IHttpClientFactory httpClientFactory)
         {
             _context = context;
             _clientFactory = httpClientFactory;
@@ -26,47 +27,43 @@ namespace ThAmCo.Stock.Controllers
 
         // GET: api/Stock
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<ProductStock>>> GetProductStock()
+        public async Task<ActionResult<IEnumerable<ProductStockDto>>> GetProductStock()
         {
-            return await _context.ProductStocks.ToListAsync();
+            return Ok(await _context.GetAll());
         }
 
         // GET: api/Stock/5
         [HttpGet("{id}")]
         public async Task<ActionResult<ProductStockDetailsDto>> Details(int id)
         {
-            var productStock = await _context.ProductStocks.FindAsync(id);
+            var productStock = await _context.GetProductStockAsync(id);
 
             if (productStock == null)
                 return NotFound();
 
-            var productPrice = await _context.Prices.FirstOrDefaultAsync(p => p.Id == productStock.PriceId);
-
-            var productDetails = new ProductStockDetailsDto()
+            return new ProductStockDetailsDto
             {
-                ProductID = productStock.ProductId,
-                Stock = productStock.Stock,
-                Price = productPrice.ProductPrice
+                ProductID = productStock.ProductStock.ProductId,
+                Stock = productStock.ProductStock.Stock,
+                Price = productStock.Price.ProductPrice
             };
-
-            return productDetails;
         }
 
         [HttpGet("PriceHistory/{id}")]
         public async Task<ActionResult<ProductStockPricingHistoryDto>> PriceHistory(int id)
         {
-            var productStock = await _context.ProductStocks.FindAsync(id);
+            var productStock = await _context.GetProductStockAsync(id);
 
             if (productStock == null)
                 return NotFound();
 
-            var productPrice = await _context.Prices.Where(p => p.ProductStockId == productStock.ProductId).ToListAsync();
+            var productPrices = _context.GetAllPrices().Result.Where(p => p.ProductStockId == productStock.ProductStock.PriceId).ToList();
 
-            var productDetails = new ProductStockPricingHistoryDto()
+            var productDetails = new ProductStockPricingHistoryDto
             {
-                ProductID = productStock.ProductId,
-                Stock = productStock.Stock,
-                Prices = productPrice
+                ProductID = productStock.ProductStock.ProductId,
+                Stock = productStock.ProductStock.Stock,
+                Prices = productPrices
             };
 
             return productDetails;
@@ -76,7 +73,8 @@ namespace ThAmCo.Stock.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutProductStock(int id, ProductStock productStock)
         {
-            if (id != productStock.Id)
+            throw new NotImplementedException();
+            /*if (id != productStock.Id)
                 return BadRequest();
 
             _context.Entry(productStock).State = EntityState.Modified;
@@ -92,44 +90,46 @@ namespace ThAmCo.Stock.Controllers
                 throw;
             }
 
-            return NoContent();
+            return NoContent();*/
         }
 
         // POST: api/Stock
         [HttpPost]
         public async Task<ActionResult<ProductStock>> PostProductStock(ProductStock productStock)
         {
-            _context.ProductStocks.Add(productStock);
+            throw new NotImplementedException();
+            /*_context.ProductStocks.Add(productStock);
             await _context.SaveChangesAsync();
 
-            return CreatedAtAction("GetProductStock", new { id = productStock.Id }, productStock);
+            return CreatedAtAction("GetProductStock", new { id = productStock.Id }, productStock);*/
         }
 
         // DELETE: api/Stock/5
         [HttpDelete("{id}")]
         public async Task<ActionResult<ProductStock>> DeleteProductStock(int id)
         {
-            var productStock = await _context.ProductStocks.FindAsync(id);
+            throw new NotImplementedException();
+            /*var productStock = await _context.ProductStocks.FindAsync(id);
             if (productStock == null)
                 return NotFound();
 
             _context.ProductStocks.Remove(productStock);
             await _context.SaveChangesAsync();
 
-            return productStock;
+            return productStock;*/
         }
 
         [HttpGet("low/{count}")]
         public async Task<ActionResult<IEnumerable<ProductStock>>> Low(int? count)
         {
-            var listToCount = await _context.ProductStocks.OrderBy(ps => ps.Stock).Take(count ?? 15).ToListAsync();
+            var listToCount = _context.GetAll().Result.OrderBy(ps => ps.ProductStock.Stock).Take(count ?? 15).ToList();
 
             return View(listToCount);
         }
 
         private bool ProductStockExists(int id)
         {
-            return _context.ProductStocks.Any(e => e.Id == id);
+            return _context.GetAll().Result.Any(e => e.ProductStock.Id == id);
         }
     }
 }
