c346ca4a6028a38437177d9dfc03726dd5e81225
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-12-24 03:38:14 +0000

    Create viewmodel for AdjustCost view.

---
+++ ThAmCo.Stock/Models/ViewModel/AdjustCostViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThAmCo.Stock.Models.ViewModel
+{
+    public class AdjustCostViewModel
+    {
+        public int Id { get; set; }
+        public double Cost { get; set; }
+    }
+}
