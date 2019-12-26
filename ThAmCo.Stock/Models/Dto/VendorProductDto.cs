dd67e4017aaaee7db8a0f647c2d00e459bed931a
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-12-26 17:35:12 +0000

    Create Dto for Vendor Products.

---
+++ ThAmCo.Stock/Models/Dto/VendorProductDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ThAmCo.Stock.Models.Dto
+{
+    public class VendorProductDto
+    {
+        public int Id { get; set; }
+        public string Ean { get; set; }
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string BrandId { get; set; }
+        public string BrandName { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool InStock { get; set; }
+        public DateTime ExpectedRestock { get; set; }
+    }
+}
