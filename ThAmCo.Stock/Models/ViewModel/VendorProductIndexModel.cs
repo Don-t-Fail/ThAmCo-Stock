c0b5150311bc345c1de85e835181109fb9240f24
Stedos <29103029+Stedoss@users.noreply.github.com>
2019-12-27 19:00:55 +0000

    Create viewmodel for VendorProducts view.

---
+++ ThAmCo.Stock/Models/ViewModel/VendorProductIndexModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using ThAmCo.Stock.Models.Dto;
+
+namespace ThAmCo.Stock.Models.ViewModel
+{
+    public class VendorProductIndexModel
+    {
+        public IEnumerable<VendorProductDto> Products { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int? BrandId { get; set; }
+        public int? CategoryId { get; set; }
+        public double? PriceLow { get; set; }
+        public double? PriceHigh { get; set; }
+    }
+}
