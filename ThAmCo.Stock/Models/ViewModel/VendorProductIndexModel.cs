2e85599fb210f708fba3631608021b31ba1af2dc
Stedos <29103029+Stedoss@users.noreply.github.com>
2020-01-01 23:47:58 +0000

    Add Vendor to VendorProductIndexModel

--- ThAmCo.Stock/Models/ViewModel/VendorProductIndexModel.cs
+++ ThAmCo.Stock/Models/ViewModel/VendorProductIndexModel.cs
@@ -8,6 +8,7 @@ namespace ThAmCo.Stock.Models.ViewModel
         public IEnumerable<VendorProductDto> Products { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Vendor { get; set; }
         public int? BrandId { get; set; }
         public int? CategoryId { get; set; }
         public double? PriceLow { get; set; }
