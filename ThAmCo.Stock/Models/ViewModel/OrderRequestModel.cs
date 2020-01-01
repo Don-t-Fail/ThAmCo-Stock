cb68f778206baac18de931dc677782a94614742e
Stedos <29103029+Stedoss@users.noreply.github.com>
2020-01-01 23:47:42 +0000

    Create model for OrderRequest.

---
+++ ThAmCo.Stock/Models/ViewModel/OrderRequestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ThAmCo.Stock.Models.ViewModel
+{
+    public class OrderRequestModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Supplier { get; set; }
+    }
+}
