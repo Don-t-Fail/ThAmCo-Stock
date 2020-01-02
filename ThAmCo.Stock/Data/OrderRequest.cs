deda629bc465ae7ed4e1de411d7d8028a8b0f1c0
Stedos <29103029+Stedoss@users.noreply.github.com>
2020-01-02 00:09:08 +0000

    Add OrderRequest type.

---
+++ ThAmCo.Stock/Data/OrderRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ThAmCo.Stock.Data
+{
+    public class OrderRequest
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public DateTime SubmittedTime { get; set; }
+        public bool Approved { get; set; }
+        public DateTime ApprovedTime { get; set; }
+        public bool Deleted { get; set; }
+    }
+}
