fa568fe94e1e59241f7295d717bc108f06c9b353
Stedos <29103029+Stedoss@users.noreply.github.com>
2020-01-03 01:13:43 +0000

    Change BrandId to be an int instead of a string.

--- ThAmCo.Stock/Models/Dto/VendorProductDto.cs
+++ ThAmCo.Stock/Models/Dto/VendorProductDto.cs
@@ -8,7 +8,7 @@ namespace ThAmCo.Stock.Models.Dto
         public string Ean { get; set; }
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
-        public string BrandId { get; set; }
+        public int BrandId { get; set; }
         public string BrandName { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
